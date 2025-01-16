ffd121f1983d6dfcb53edc2c18850b7f76e8ae8f
JaredMZ <jaredcv04@gmail.com>
2025-01-15 18:10:01 -0600

    main setup

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expenses Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Pavanam'),
+      home: HomeScreen(),
+    );
+  }
+}
